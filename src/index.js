944bd4cab321d367ce1e80980c2cef31adcbd908
Faiza-Hoosein <faiza.hoosein@gmail.com>
2025-06-30 23:36:44 +0200

    Updated javacsript

--- src/index.js
+++ src/index.js
@@ -57,9 +57,34 @@ function handleSearchSubmit(event) {
     searchCity(city);
   }
 }
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15℃</strong>
+          </div>
+          <div class="weather-forecast-temperature">9℃</div>
+        </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 
 document
   .querySelector("#search-form")
   .addEventListener("submit", handleSearchSubmit);
 
 searchCity("Johannesburg");
+displayForecast();
